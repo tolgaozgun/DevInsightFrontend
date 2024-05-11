05a369720aea2144e555991374d2ebc5faa52e3e
Tolga Özgün <tolgaozgunn@gmail.com>
2024-05-11 16:00:33 +0300

    Fix host setup

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,12 +1,34 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  base: '/',
+  plugins: [react()],
+  // Required for Docker containers
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5173, // you can replace this port with any port
+  },
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './vitest.setup.mjs',
+  },
+  // Docker container end
+  build: {
+    rollupOptions: {
+      plugins: [
+        copy({
+          targets: [
+            { src: '404.html', dest: 'dist' }, // configure the source and destination paths
+          ],
+          hook: 'writeBundle', // copy after bundle has been written
+        }),
+      ],
+    },
   },
 });
